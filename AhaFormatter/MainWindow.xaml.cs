e8bafdc23f9cade245d3d52e1045f527620528e5
insrt <insrtiscool@gmail.com>
2022-05-18 16:07:51 +0300

    Added youtube playlist auto creation of parsed csv list

--- AhaFormatter/MainWindow.xaml.cs
+++ AhaFormatter/MainWindow.xaml.cs
@@ -8,6 +8,13 @@ using System.Windows.Forms;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Net.Http;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.Threading;
+using Google;
 
 namespace DoresoFormatter
 {
@@ -16,13 +23,14 @@ namespace DoresoFormatter
         readonly string idle = new("Awaiting input");
         string csvPath = new("");
         string outputPath = new("");
-        YoutubeAPI api;
+        string playlistTitle = new("");
+        List<String> results;       
 
         public MainWindow()
         {
             InitializeComponent();
             Console.Text = idle;
-            api = new();
+            results = new();
         }
 
         private void SelectFileClick(object sender, RoutedEventArgs e)
@@ -67,11 +75,10 @@ namespace DoresoFormatter
         private async void ParseCSV(object sender, RoutedEventArgs e)
         {
             Console.AppendText(Environment.NewLine);
-            List<String> results = new();
             using (var reader = new StreamReader(csvPath))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                var records = csv.GetRecords<Song>();
+                IEnumerable<Song> records = csv.GetRecords<Song>();
                 foreach (var record in records)
                 {
                     string youtubeUrl = await GetYoutubeURLAsync(record.Detail_URL);
@@ -81,6 +88,7 @@ namespace DoresoFormatter
                 }
             }
             string fileName = "DoresoParse - " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss") + ".txt";
+            playlistTitle = fileName.Replace(".txt", "");
             if (!File.Exists(outputPath))
             {
                 using StreamWriter stream = File.CreateText(outputPath + "\\" + fileName);
@@ -88,7 +96,9 @@ namespace DoresoFormatter
                 {
                     await stream.WriteLineAsync(result);
                 }
-                Console.AppendText(Environment.NewLine + "Successfully wrote " + results.Count + " songs to " + fileName + " from CSV " + csvPath);
+                Console.AppendText(Environment.NewLine + "Successfully wrote " + results.Count + " songs to " + fileName + " from CSV " + csvPath + 
+                    Environment.NewLine + "Create new playlist available");
+                CreatePlaylist.IsEnabled = true;
             }
         }
 
@@ -114,20 +124,20 @@ namespace DoresoFormatter
             Console.Text = idle;
         }
 
-        private void pseudoConsole_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void Console_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             Console.ScrollToEnd();
         }
 
         private async Task<string> GetYoutubeURLAsync(string? link)
         {
-            Console.AppendText("Getting Doreso youtube link and transforming");
+            Console.AppendText(Environment.NewLine + "Getting Doreso youtube link and transforming");
             string youtubeURL = "";
             HttpClient cli = new(new HttpClientHandler() { UseDefaultCredentials = true });
             HttpResponseMessage response = await cli.GetAsync(link);
             var data = await response.Content.ReadAsStreamAsync();
             string html = String.Empty;
-            using (StreamReader sr = new StreamReader(data))
+            using (StreamReader sr = new(data))
             {
                 html = sr.ReadToEnd();
             }
@@ -143,6 +153,81 @@ namespace DoresoFormatter
                 youtubeURL = url.Replace("/embed/", "/watch?v=");
             }
             return youtubeURL;
+        }
+
+        private async void CreatePlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            Console.AppendText(Environment.NewLine + "Creating playlist " + playlistTitle);
+            try
+            {
+                await CreateNewPlaylist(results, playlistTitle);
+            }
+            catch (AggregateException ex)
+            {
+                Console.AppendText( Environment.NewLine + "Playlist creation failed.");
+                foreach (var error in ex.InnerExceptions)
+                {
+                    Console.AppendText(Environment.NewLine + "Error: " + error.Message);
+                }
+            }
+        }
+
+        public async Task CreateNewPlaylist(List<string> playlist, string playlistTitle)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(@"C:\Users\insrt\source\repos\AhaFormatter\AhaFormatter\client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                //Will throw null reference exception if client_secrets.json path is not correct.
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    new[] { YouTubeService.Scope.Youtube },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(this.GetType().ToString())
+                );
+            }
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            Playlist newPlaylist = new();
+            newPlaylist.Snippet = new PlaylistSnippet();
+            newPlaylist.Snippet.Title = playlistTitle;
+            newPlaylist.Snippet.Description = "A playlist created with my DoresoFormatter";
+            newPlaylist.Status = new PlaylistStatus();
+            newPlaylist.Status.PrivacyStatus = "private";
+            newPlaylist = await youtubeService.Playlists.Insert(newPlaylist, "snippet,status").ExecuteAsync();
+
+            Console.AppendText(Environment.NewLine + "Created new playlist " + playlistTitle);
+
+            foreach (string song in playlist)
+            {
+                Regex reg = new(@"youtube.com/watch\?v=[A-Za-z0-9\-_]{11}");
+                if (reg.IsMatch(song))
+                {
+                    try
+                    {
+                        string videoId = reg.Match(song).Value;
+                        var idOnly = videoId.Replace("youtube.com/watch?v=", "");
+                        PlaylistItem songItem = new();
+                        songItem.Snippet = new PlaylistItemSnippet();
+                        songItem.Snippet.PlaylistId = newPlaylist.Id;
+                        songItem.Snippet.ResourceId = new ResourceId();
+                        songItem.Snippet.ResourceId.Kind = "youtube#video";
+                        songItem.Snippet.ResourceId.VideoId = idOnly;
+                        songItem = await youtubeService.PlaylistItems.Insert(songItem, "snippet").ExecuteAsync();
+                        Console.AppendText(Environment.NewLine + "Added " + songItem.Snippet.Title + " to " + playlistTitle);
+                    }
+                    catch (GoogleApiException ex)
+                    {
+                        Console.AppendText(Environment.NewLine + "error: " + ex.Message);
+                    }
+                }
+            }
+            Console.AppendText(Environment.NewLine + "Finished creating playlist.");
         }
     }
 }
